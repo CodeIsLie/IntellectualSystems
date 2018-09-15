9e4ad86f0fd1392ca142c7a4d8a8e8586144f8f5
CodeIsLie <ilya1757@gmail.com>
2018-09-15 20:22:28 +0300

    first working version of fifteens, simple A*

--- lab02/fifteens.cpp
+++ lab02/fifteens.cpp
@@ -1,53 +1,145 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <queue>
+#include <unordered_set>
 
 using namespace std;
 
 class Node{
-    vector<vector<int>> grid;
+public:
+    Node* parent;
+    vector<size_t> grid;
     int x;
     int y;
-    Node(vector<vector<int>>& grid, int x, int y){
+    Node(vector<size_t> grid, int x, int y, Node* parent= nullptr){
         this -> grid = move(grid);
         this -> x = x;
         this -> y = y;
-//        for (int i = 0; i < 4; ++i){
-//            for (int j = 0; j < 4; ++j){
-//                if (grid[i][j] == 0) {
-//                    x = j;
-//                    y = i;
-//                }
-//
-//            }
-//        }
+        this->parent = parent;
     }
 
     Node* getNode(int newX, int newY){
-        vector<vector<int>> newGrid = grid;
-        newGrid[x][y] = newGrid[newX][newY];
-        newGrid[newX][newY] = 0;
-        return new Node(newGrid, newX, newY);
+        vector<size_t> newGrid = grid;
+        newGrid[x+ y*4] = newGrid[newX + newY*4];
+        newGrid[newX + newY*4] = 0;
+        return new Node(newGrid, newX, newY, this);
     }
 
     vector<Node*> getMoves(){
         vector<Node*> moves;
         if (x > 0){
-            int newX = x-1;
-            int newY = y;
-            moves.push_back(getNode(newX, newY));
+            moves.push_back(getNode(x-1, y));
         }
-
         if (x < 3){
-            int newX = x+1;
-            int newY = y;
-            moves.push_back(getNode(newX, newY));
+            moves.push_back(getNode(x+1, y));
+        }
+        if (y > 0){
+            moves.push_back(getNode(x, y-1));
+        }
+        if (y < 3){
+            moves.push_back(getNode(x, y+1));
         }
 
-        return moves;
+        return move(moves);
     }
 };
 
+void showGrid(Node* node){
+    vector<size_t >& grid = node->grid;
+    for (int i = 0; i < grid.size(); ++i){
+        if (i%4 == 0)
+            cout << endl;
+        cout << grid[i] << ' ';
+    }
+    cout << endl;
+}
+
+void showPath(Node* node){
+    while (node != nullptr){
+        showGrid(node);
+        node = node -> parent;
+    }
+}
+
+struct GridEquals{
+public:
+    bool operator()(const Node* node1, const Node* node2) const{
+        return node1->grid == node2->grid;
+    }
+};
+
+struct GridHash{
+public:
+    size_t operator()(const Node* node) const{
+        size_t hash1 = 0;
+        size_t hash2 = 0;
+        const vector<size_t>& grid = node->grid;
+        int i;
+        for (i = 0; i < 8; ++i){
+            hash1 <<= 2;
+            hash1 += grid[i];
+        }
+        for (; i< 16; ++i){
+            hash2 <<= 2;
+            hash2 += grid[i];
+        }
+        return hash1 ^ hash2;
+    }
+};
+
+int Manhetten(Node* node){
+    vector<size_t>& grid = node->grid;
+    int sum = 0;
+    for (int i = 0; i < 15; ++i){
+        size_t currentI = grid[i]-1;
+        int dx = abs((i % 4) - int(currentI % 4));
+        int dy = abs(i/4 - int(currentI/4));
+        sum += dx + dy;
+    }
+    return sum;
+}
+
+bool isGoal(vector<size_t> grid){
+    for (int i = 0; i < grid.size()-1; ++i){
+        if (i+1 != grid[i])
+            return false;
+    }
+    return grid[grid.size()-1] == 0;
+}
+
+Node* AStar(Node* node, function<int(Node*)> h){
+    unordered_set<Node*, GridHash, GridEquals> used;
+
+    priority_queue<pair<int, Node*>> qq;
+    qq.emplace(-h(node), node);
+
+    while (!qq.empty()){
+        auto node = qq.top();
+        used.insert(node.second);
+        qq.pop();
+
+        if (isGoal(node.second->grid)){
+            return node.second;
+        }
+
+        int pathLength = -node.first - h(node.second);
+        for (Node* to: node.second->getMoves()){
+            showGrid(to);
+            if (used.find(to) == used.end()) {
+                size_t heur = h(to) + pathLength + 1;
+                qq.emplace(-heur, to);
+            }
+        }
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<size_t> startGrid = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15};
+    Node* start = new Node(startGrid, 2, 3);
+    size_t hash = GridHash()(start);
+    cout << hash << endl;
+    Node* answer = AStar(start, Manhetten);
+    showPath(answer);
     return 0;
 }
