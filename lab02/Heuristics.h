2e16efd1981118765288b8954e940e9d57b8dd02
CodeIsLie <ilya1757@gmail.com>
2018-09-25 21:41:54 +0300

    move Heuristics to new files

---
+++ lab02/Heuristics.h
@@ -0,0 +1,13 @@
+//
+// Created by Ilya on 25.09.2018.
+//
+
+#ifndef LAB02_HEURISTICS_H
+#define LAB02_HEURISTICS_H
+
+#include "Node.h"
+
+int Manhetten(Node* node);
+int Conflicts(Node* node);
+
+#endif //LAB02_HEURISTICS_H
