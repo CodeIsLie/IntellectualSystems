50675537b6e87bd2c2ebe3ac4aea431d5d9ac353
Ilya Yakubovsky <ilya1757@gmail.com>
2018-12-25 11:08:19 +0300

    aiml file and python for aiml, don't work with ru input

---
+++ telegram_bot/aiml_bot.py
@@ -0,0 +1,11 @@
+import aiml
+
+# Create the kernel and learn AIML files
+kernel = aiml.Kernel()
+kernel.learn("basic_chat.aiml")
+kernel.respond("load aiml b")
+
+# Press CTRL-C to break this loop
+while True:
+    print("Enter your message >> ")
+    print(kernel.respond(input()))
